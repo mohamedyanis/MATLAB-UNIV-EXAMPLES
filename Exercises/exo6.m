d87f1e9da10ed3ae863a4c050fb0cf306aea3211
Mohamed Yanis HIOU <61564196+mohamedyanis@users.noreply.github.com>
2021-04-25 17:09:32 +0100

    Update exo6.m

--- Exercises/exo6.m
+++ Exercises/exo6.m
@@ -17,10 +17,10 @@ b = input('Please enter the number "B": ');
 
 
 if rem(a,b) == 0
-  fprintf('The number %d is divisible by %d',a,b);
+  fprintf('The number %d is divisible by %d\n',a,b);
   
 else
-  fprintf('The number %d is not divisible by %d', a,b);
+  fprintf('The number %d is not divisible by %d\n', a,b);
 end
 
 
