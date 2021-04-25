5d5bf85ea589f92ae9fcaebc8585aa374caecff6
Mohamed Yanis HIOU <61564196+mohamedyanis@users.noreply.github.com>
2021-04-25 17:14:46 +0100

    Update exo3.m

--- Exercises/exo3.m
+++ Exercises/exo3.m
@@ -11,16 +11,17 @@
 
 %###########################################################
 
+num1 = input('Please Enter An Integer: ');
+num2 = input('Please Enter A Second Integer: ');
 
-% you can work an input value!
+quorem_num = 0;
+rest_num = num1;
+while (rest_num >= num2)
+    quorem_num = quorem_num+1;
+    rest_num = rest_num-num2;
+end
+fprintf('The quorem of 10 and 3 is: %d\n',quorem_num');
+fprintf('The rest of the quorem of 10 and 3 is: %d\n',rest_num);
 
-[rest_num, quorem_num] = quorem(sym(10),sym(3));  %#### the sym function convert to the symbolic variable!
-                                                  %#### the 'quorem()' function give us the quorem and the rest of the division!
 
-fprintf('The quorem of 10 and 3 is: %d',quorem_num);
-
-fprintf('The rest of the quorem of 10 and 3 is: %d',rest_num);
-
-
-
-%##### end of program
+%##### end of program!
