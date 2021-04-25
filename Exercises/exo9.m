0a9e7b221ca58ff452ba5d6a4cabd8aacf8398c1
Mohamed Yanis HIOU <61564196+mohamedyanis@users.noreply.github.com>
2021-04-25 17:07:55 +0100

    Update exo9.m

--- Exercises/exo9.m
+++ Exercises/exo9.m
@@ -1,6 +1,6 @@
 %###########################################################
 
-%exo 8
+%exo 9
 %Algorithm which allows the user to enter a character ending in '+', and which displays at the end the number of appearances of the letter 'A'
 %by Mohamed Yanis Hiou
 %Automation L2, Group 2
@@ -10,39 +10,24 @@
 %@medyanis_hiou
 
 %###########################################################
-% First Methode:
 
-prompt = 'Enter a string: ';
-str = input (prompt, "s");
-d = count(str, "a");
-fprintf('Your string has %d "A"', d);
+char phrase[200];
+A=0;
 
-%#####end of program 1
-
-
-
-%###########################################################
-% Seconde Methode: (By Ahmed Bentaleb: https://github.com/bentalebahmed)
-
-import mlreportgen.dom.*;
-
-test = "hello world!";
-
-list = Document('List','html');
-
-n = 0;
-
-for i in test
-append(list, i);
+phrase=input('Please Enter A Phrase: ','s');
+word(length(phrase)+1)="*";
+s=length(phrase);                                  %#### The 'lenghth' function counts how many letter in a word
+for i=1:s
+  small_check = strcmp(phrase(i),'a');             %#### The 'strcmp' checks if there is a letter 'x' in a word
+  if small_check == 1
+    A++;
+  end
+  
+  cap_check = strcmp(phrase(i),'A');
+  if cap_check == 1
+    A++;
+  end 
 end
+fprintf('There Are %d A\n',A);
 
-close(list);
-
-for j in list
-if strcmp(j,'o')
-n = n +1;
-end
-end
-print(n);
-
-%#####end of program 2
+%#####end of program 
